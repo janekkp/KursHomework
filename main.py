66f12e9ddbcbddf16e0923b252212403f56ee8c8
janekkp <37982410+janekkp@users.noreply.github.com>
2020-04-02 19:42:11 +0200

    Initial commit

--- main.py
+++ main.py
@@ -6,3 +6,23 @@ app = FastAPI()
 @app.get('/')
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+
+@app.post('/method')
+def method_post():
+    return {"method": "POST"}
+
+
+@app.get('/method')
+def method_get():
+    return {'method': "GET"}
+
+
+@app.put('/method')
+def method_put():
+    return {'method': "PUT"}
+
+
+@app.delete('/method')
+def method_delate():
+    return {"method": "DELETE"}
