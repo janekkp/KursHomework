bddd5440654224fd64e477748544644f94167639
janekkp <37982410+janekkp@users.noreply.github.com>
2020-04-27 12:11:12 +0200

    zadanie 5

--- main.py
+++ main.py
@@ -2,27 +2,20 @@ from fastapi import FastAPI, Depends, HTTPException, status, Cookie, Request
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import Response, JSONResponse, RedirectResponse
+from fastapi.responses import Response
 from hashlib import sha256
-import http
-
-
-
 
 app = FastAPI()
 app.counter = 0
-app.data = []
+app.patients = {}
 app.secret = "secret"
 app.users = {"trudnY": "PaC13Nt", "admin": "admin"}
 app.session_tokens = []
 
 
-
 @app.get('/')
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
-
 
 
 @app.post('/method')
@@ -41,34 +34,63 @@ def method_put():
 
 
 @app.delete('/method')
-def method_delate():
+def method_delete():
     return {"method": "DELETE"}
 
 
 class Patient(BaseModel):
     name: str
-    surename: str
+    surname: str
 
 
 def counter():
     app.counter += 1
-    return (int(app.counter) - 1)
+    return int(app.counter) - 1
 
-@app.post("/patient")
-def make_item(item: Patient):
-    tmp_json = jsonable_encoder(item)
-    app.data.append(tmp_json)
-    new_dict = {"id": counter(), "patient": tmp_json}
-    json_compatible_item_data = jsonable_encoder(new_dict)
-    return JSONResponse(content=json_compatible_item_data)
+# @app.post("/patient")
+# def make_item(item: Patient):
+#     tmp_json = jsonable_encoder(item)
+#     app.data.append(tmp_json)
+#     new_dict = {"id": counter(), "patient": tmp_json}
+#     json_compatible_item_data = jsonable_encoder(new_dict)
+#     return JSONResponse(content=json_compatible_item_data)
 
 
-@app.get("/patient/{pk}")
-def show_patient(pk: int):
-    try:
-        return app.data[pk]
-    except:
-        return Response(status_code=http.HTTPStatus.NO_CONTENT)
+@app.post('/patient')
+def add_patient(response: Response, patient: Patient, session_token: str = Cookie(None)):
+    if session_token not in app.session_tokens:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    id = counter()
+    app.patients[id] = patient.dict()
+    response.set_cookie(key="session_token", value=session_token)
+    response.status_code = 302
+    response.headers["Location"] = f'/patient/{id}'
+
+
+@app.get('/patient')
+def patients(session_token: str = Cookie(None)):
+    if session_token not in app.session_tokens:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    return app.patients
+
+
+@app.get("/patient/{id}")
+def show_patient(id: int, response: Response, session_token : str = Cookie(None)):
+    if session_token not in app.session_tokens:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    response.set_cookie(key="session_token", value=session_token)
+    if id in app.patients.keys():
+        return app.patients[id]
+    return status.HTTP_204_NO_CONTENT
+
+
+@app.delete('/patinet/{id}')
+def delete_patient(id: int, response: Response, session_token : str = Cookie(None)):
+    if session_token not in app.session_tokens:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    app.patients.pop(id, None)
+    return status.HTTP_204_NO_CONTENT
+
 
 
 templates = Jinja2Templates(directory="templates")
@@ -81,7 +103,6 @@ def welcoming(request: Request, session_token: str = Cookie(None)):
     return templates.TemplateResponse("item.html", {"request": request, "user": "trudnY"})
 
 
-
 @app.post("/login")
 def login(response: Response, credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
     if credentials.username in app.users and app.users[credentials.username] == credentials.password:
@@ -90,6 +111,7 @@ def login(response: Response, credentials: HTTPBasicCredentials = Depends(HTTPBa
         app.session_tokens.append(session_token)
         response.status_code = 302
         response.headers["Location"] = '/welcome'
+        print(session_token)
         return response
     else:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
